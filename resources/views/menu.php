22b0d7c01d770bed562ed3aae3d503a6ec19943b
shahov.e <shahov.e@oneway.su>
2022-09-21 07:33:01 +0300

    Lesson 1.

---
+++ resources/views/menu.php
@@ -0,0 +1,11 @@
+<ul>
+    <li>
+        <a href="/"><p>Главная</p></a>
+    </li>
+    <li>
+        <a href="/about"><p>О проекте</p></a>
+    </li>
+    <li>
+        <a href="/news"><p>Новости</p></a>
+    </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/news', function () {
+    return view('news');
 });
