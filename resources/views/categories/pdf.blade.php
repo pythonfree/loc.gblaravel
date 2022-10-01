fbc429c6438eecf4a970b60673e0f808e974aa61
shahov.e <shahov.e@oneway.su>
2022-10-01 22:41:13 +0300

    Lesson 4.

--- resources/views/categories/pdf.blade.php
+++ resources/views/categories/pdf.blade.php
@@ -11,7 +11,6 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
                     <div class="card-body">
                         <p>Новости категории - {{ $title }}:</p>
                         @forelse($news as $article)
