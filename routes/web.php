9cc527ec9c0c99b81d6d51022a124dcc8552b276
shahov.e <shahov.e@oneway.su>
2022-10-12 13:53:42 +0300

    Добавил вручную роуты для авторизации и регистрации и входа.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Http\Controllers\Admin\AdminProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
@@ -26,10 +27,10 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/auth', 'auth')->name('auth');
 Route::view('/vue', 'vue')->name('vue');
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+//Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::name('news.')
-    ->prefix('news')
+Route::prefix('news')
+    ->name('news.')
     ->group(function () {
         Route::get('/', [NewsIndexController::class, 'index'])->name('index');
         Route::get('/one/{news}', [NewsIndexController::class, 'show'])->name('show');
@@ -40,14 +41,21 @@ Route::name('news.')
             });
     });
 
-
-Route::prefix('admin')
+Route::middleware(['auth', 'isAdmin'])
+    ->prefix('admin')
     ->name('admin.')
     ->group(function () {
         Route::get('/image', [AdminIndexController::class, 'imageDownload'])->name('image');
         Route::match(['get', 'post'], '/download', [AdminIndexController::class, 'download'])->name('download');
+        Route::match(['get', 'post'], '/profile', [AdminProfileController::class, 'update'])->name('updateProfile');
         Route::resource('/news', AdminNewsController::class);
         Route::resource('/categories', AdminCategoriesController::class);
     });
 
-Auth::routes();
+//Auth::routes();
+Auth::routes(['register' => false]);
+//Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+//Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+//Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+//Route::get('/register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+
