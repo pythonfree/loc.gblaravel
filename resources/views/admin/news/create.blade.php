a82571a49e6d74d2d56251e4e066d87a4a40b21f
shahov.e <shahov.e@oneway.su>
2022-10-19 11:24:09 +0300

    Lesson 10.

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -110,17 +110,14 @@
             </div>
         </div>
     </div>
-    <script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+    <script src="{{ asset('assets/js/ckeditor/ckeditor.js') }}"></script>
     <script>
-        let csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
         var options = {
             filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-            filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=' + csrf,
+            filebrowserImageUploadUrl: "/laravel-filemanager/upload?type=Images&_token={{ csrf_token() }}",
             filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-            filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token=' + csrf,
+            filebrowserUploadUrl: "/laravel-filemanager/upload?type=Files&_token={{ csrf_token() }}",
         };
-    </script>
-    <script>
         CKEDITOR.replace('articleText', options);
     </script>
 @endsection
